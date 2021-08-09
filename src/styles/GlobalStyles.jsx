0f248a3157a0085d10bf9ddc008943e23f4f7895
nynon <nynonmaslo@gmail.com>
2021-08-09 11:05:38 +0200

    added global theme

--- src/styles/GlobalStyles.jsx
+++ src/styles/GlobalStyles.jsx
@@ -1,11 +1,13 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyles = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css2?family=Mulish:wght@200&display=swap');
   * {
     -webkit-font-smoothing: antialiased;
     box-sizing: border-box;
     margin: 0;
     padding: 0;
+    font-family: ${(props) => props.theme.fontFamily.main}
   }
 `;
 
