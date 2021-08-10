9631e3a4032f5ecc6f6232097a272948c90364fa
alojzy231 <alojzy231@gmail.com>
2021-08-10 11:57:14 +0200

    Added controlled input field

--- src/components/formElements/input/index.jsx
+++ src/components/formElements/input/index.jsx
@@ -4,15 +4,35 @@ import StyledInput from './styledInput';
 import Icon from './icon';
 import StyledInputContainer from './styledInputContainer';
 
-export default function InputContainer({ placeholder, disabled, withIcon, hasError }) {
+export default function InputContainer({
+  placeholder,
+  disabled,
+  isControlled,
+  withIcon,
+  hasError,
+  value,
+  onChange,
+}) {
   return (
     <StyledInputContainer withIcon={withIcon}>
-      <StyledInput
-        placeholder={placeholder}
-        disabled={disabled}
-        hasError={hasError}
-        withIcon={withIcon}
-      />
+      {isControlled ? (
+        <StyledInput
+          placeholder={placeholder}
+          disabled={disabled}
+          hasError={hasError}
+          withIcon={withIcon}
+          onChange={onChange}
+          value={value}
+        />
+      ) : (
+        <StyledInput
+          placeholder={placeholder}
+          disabled={disabled}
+          hasError={hasError}
+          withIcon={withIcon}
+        />
+      )}
+
       {withIcon && <Icon hasError={hasError} disabled={disabled} />}
     </StyledInputContainer>
   );
@@ -21,13 +41,19 @@ export default function InputContainer({ placeholder, disabled, withIcon, hasErr
 InputContainer.propTypes = {
   placeholder: PropTypes.string,
   disabled: PropTypes.bool,
+  isControlled: PropTypes.bool,
   withIcon: PropTypes.bool,
   hasError: PropTypes.bool,
+  value: PropTypes.string,
+  onChange: PropTypes.func,
 };
 
 InputContainer.defaultProps = {
   placeholder: '',
   disabled: false,
+  isControlled: false,
   withIcon: false,
   hasError: false,
+  value: '',
+  onChange: () => {},
 };
