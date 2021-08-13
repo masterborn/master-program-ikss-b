1b442f155d9e083c284c147a60baad962be6eb79
alojzy231 <alojzy231@gmail.com>
2021-08-13 16:47:34 +0200

    Fixed data type
    Navbar height was passed as ' px' and it made a problem

--- src/components/layout/navbar/contactFormButton.jsx
+++ src/components/layout/navbar/contactFormButton.jsx
@@ -1,10 +1,11 @@
 const handleClick = (currPathname, navbarHeight) => {
   if (currPathname === '/') {
     const contactForm = document.getElementById('contactForm');
+    const navbarOffset = Number(navbarHeight.replace(/px$/, ''));
 
     if (contactForm) {
       const contactFormPosition = contactForm.getBoundingClientRect().top;
-      window.scrollBy(0, contactFormPosition - navbarHeight);
+      window.scrollBy(0, contactFormPosition - navbarOffset);
     }
   } else {
     // do something
