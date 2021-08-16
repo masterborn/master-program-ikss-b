159e9e06d96627b2d32cdc495395a99637537a7a
alojzy231 <alojzy231@gmail.com>
2021-08-16 15:42:10 +0200

    Added footer

---
+++ src/components/layout/footer/Socials.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { FbIcon, IgIcon, YtIcon, InIcon } from '../../icons/socialRegular';
+
+export default function Socials({ socialsLinks }) {
+  return (
+    <div className="socials">
+      <a href={socialsLinks.fb}>
+        <FbIcon />
+      </a>
+      <a href={socialsLinks.ig}>
+        <IgIcon />
+      </a>
+      <a href={socialsLinks.yt}>
+        <YtIcon />
+      </a>
+      <a href={socialsLinks.in}>
+        <InIcon />
+      </a>
+    </div>
+  );
+}
+
+Socials.propTypes = {
+  socialsLinks: PropTypes.shape({
+    fb: PropTypes.string,
+    ig: PropTypes.string,
+    yt: PropTypes.string,
+    in: PropTypes.string,
+  }).isRequired,
+};
