962be533ee406b81531b25ce490d34136a2f5ec9
nynon <nynonmaslo@gmail.com>
2021-08-16 15:42:57 +0200

    added api request timeout

--- src/api/index.js
+++ src/api/index.js
@@ -1,15 +1,19 @@
 import data from './data.json';
 
 export default async function getPagesData() {
-  const res = await fetch(
-    `https://cdn.contentful.com/spaces/${process.env.SPACE_ID}/environments/${process.env.ENV_ID}/entries?access_token=${process.env.TOKEN}`,
-  );
+  const apiEndPoint = `https://cdn.contentful.com/spaces/${process.env.SPACE_ID}/environments/${process.env.ENV_ID}/entries?access_token=${process.env.TOKEN}`;
+  const controller = new AbortController();
+  setTimeout(() => controller.abort(), 5000);
+
+  const res = await fetch(apiEndPoint, { signal: controller.signal });
   if (res.status !== 200) {
     throw new Error(`Wrong Status Code: ${res.status}: ${res.statusText}`);
   }
+
   const resJSON = await res.json();
   if (!resJSON) throw new Error("Response doesnt't contain JSON package");
   if (!resJSON.includes.Asset) throw new Error('JSON data is corrupted: No assets present');
+
   return resJSON;
 }
 
