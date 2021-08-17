402cddf2dfc3f54317372951e51f28f80c067a93
alojzy231 <alojzy231@gmail.com>
2021-08-17 14:02:32 +0200

    Added homepage top section

---
+++ src/components/home/topSection/UnstyledTopSection.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Header1 } from '../../typography/headers';
+import { ParagraphBody } from '../../typography/paragraphs';
+import SecondaryButton from '../../buttons/secondaryButton';
+import Socials from './Socials';
+
+const MOCKUP = {
+  title: 'Przykładowy nagłówek IKSS',
+  text: 'Urna, mi condimentum amet, consectetur mauris tincidunt gravida aenean. Dignissim in sit arcu nam. Ultrices integer odio feugiat vulputate.',
+  image1:
+    '//videos.ctfassets.net/n21y2i4hkj4h/4fPIBredtzYNienv1X0MSk/93a72f310b4c8a02de8428cccd288ceb/na_strone_ikss.mp4',
+};
+const MOCK_SOCIALS = {
+  fb: 'https://www.facebook.com/',
+  ig: 'https://www.instagram.com/',
+  yt: 'https://www.youtube.com/',
+  in: 'https://www.linkedin.com/',
+};
+
+export default function UnstyledTopSection({ className }) {
+  const handleClick = () => {};
+
+  return (
+    <section className={`${className} hideNavSocials`}>
+      <div className="main-content">
+        <div className="left-side">
+          <Header1>{MOCKUP.title}</Header1>
+          <ParagraphBody>{MOCKUP.text}</ParagraphBody>
+          <SecondaryButton isBig onClick={handleClick}>
+            Skontaktuj się
+          </SecondaryButton>
+        </div>
+        <video className="video" src={`https:${MOCKUP.image1}`} muted autoPlay loop />
+      </div>
+
+      <Socials socialsLinks={MOCK_SOCIALS} />
+    </section>
+  );
+}
+
+UnstyledTopSection.propTypes = {
+  className: PropTypes.string.isRequired,
+};
---
+++ src/components/home/topSection/index.jsx
@@ -0,0 +1,69 @@
+import styled from 'styled-components';
+import TopSection from './UnstyledTopSection';
+
+const StyledTopSection = styled(TopSection)`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  .main-content {
+    display: flex;
+    flex-direction: row;
+    align-items: flex-start;
+    justify-content: flex-end;
+    .left-side {
+      min-width: 416px;
+      margin: 0 6.59% 20px 8%;
+      padding-top: 77px;
+      h1 {
+        color: ${(props) => props.theme.color.navy};
+      }
+
+      p {
+        width: 384px;
+        margin: 32px 0;
+
+        color: ${(props) => props.theme.color.steel};
+      }
+    }
+    .video {
+      width: 56%;
+    }
+  }
+  .socials {
+    width: 1037px;
+    height: 137px;
+
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    padding-right: 120px;
+    padding-left: 56px;
+
+    background-color: ${(props) => props.theme.color.white};
+
+    box-shadow: 3.38443px 55.8976px 80px rgba(97, 121, 139, 0.07),
+      1.71337px 28.2982px 34.875px rgba(97, 121, 139, 0.04725),
+      0.676885px 11.1795px 13px rgba(97, 121, 139, 0.035),
+      0.148069px 2.44552px 4.625px rgba(97, 121, 139, 0.02275);
+    border-radius: 16px 0px 0px 16px;
+
+    a {
+      height: 48px;
+      display: flex;
+      flex: row;
+      align-items: center;
+
+      text-decoration: none;
+
+      color: ${(props) => props.theme.color.navy};
+      svg {
+        transform: scale(2);
+        margin-right: 24px;
+        margin-left: 24px;
+      }
+    }
+  }
+`;
+
+export default StyledTopSection;
