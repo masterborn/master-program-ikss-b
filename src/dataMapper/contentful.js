6eb17f9bca87c8d7ad5a99fa996f3ac9c596ad03
nynon <nynonmaslo@gmail.com>
2021-08-17 19:42:18 +0200

    changed file name to a more meanigful one, changed function names to be more precise, removed nested function declaration

--- src/dataMapper/contentful.js
+++ src/dataMapper/contentful.js
@@ -1,13 +1,13 @@
+function extractAsset(itemData, assets) {
+  const assetId = itemData.sys.id;
+  const asset = assets.find((collectionAsset) => collectionAsset.sys.id === assetId);
+  const assetData = { url: asset.fields.file.url, title: asset.fields.title };
+  return assetData;
+}
+
 function addAssetsToItems(data) {
   const { items } = data;
   const assets = data.includes.Asset;
-
-  const fetchAsset = (itemData) => {
-    const assetId = itemData.sys.id;
-    const asset = assets.find((collectionAsset) => collectionAsset.sys.id === assetId);
-    const assetData = { url: asset.fields.file.url, title: asset.fields.title };
-    return assetData;
-  };
 
   items.forEach((item) => {
     const itemData = item.fields;
@@ -15,19 +15,20 @@ function addAssetsToItems(data) {
     Object.keys(itemData).forEach((contentKey) => {
       const contentData = itemData[contentKey];
       if (contentData.sys?.linkType === 'Asset') {
-        const asset = fetchAsset(contentData);
+        const asset = extractAsset(contentData, assets);
         itemData[contentKey] = asset;
       }
     });
   });
   return data;
 }
+
 function extractItems(data) {
   const collection = addAssetsToItems(data);
   return collection.items;
 }
 
-function sortIntoPages(basicContent) {
+function mapByPages(basicContent) {
   const pagesCollection = {};
   basicContent.forEach((itemData) => {
     const { page } = itemData || 'common';
@@ -37,7 +38,7 @@ function sortIntoPages(basicContent) {
   return pagesCollection;
 }
 
-function sortToContentType(items) {
+function mapByContentType(items) {
   const sortedItems = {};
   items.forEach((item) => {
     const itemData = item.fields;
@@ -46,15 +47,15 @@ function sortToContentType(items) {
   });
   return sortedItems;
 }
-function sortCollection(collection) {
-  const sortedCollection = sortToContentType(collection);
-  const pagesCollection = sortIntoPages(sortedCollection.basicContent);
-  sortedCollection.basicContent = pagesCollection;
-  return sortedCollection;
+function mapItemsCollection(collection) {
+  const mappedCollection = mapByContentType(collection);
+  const pagesCollection = mapByPages(mappedCollection.basicContent);
+  mappedCollection.basicContent = pagesCollection;
+  return mappedCollection;
 }
 
 export default function mapData(data) {
   const itemCollection = extractItems(data);
-  const sortedCollection = sortCollection(itemCollection);
-  return sortedCollection;
+  const mappedCollection = mapItemsCollection(itemCollection);
+  return mappedCollection;
 }
