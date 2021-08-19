879ed3d6f8ef0faf6396cb29a336a498792443e0
alojzy231 <alojzy231@gmail.com>
2021-08-19 12:59:01 +0200

    Added 'terms' subpage

---
+++ src/pages/terms.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function terms() {
+  return (
+    <div>
+      <ol>
+        Do you agree to:
+        <li>Being super cool</li>
+        <li>Behave nicely</li>
+      </ol>
+    </div>
+  );
+}
