24319e8575c6f47035296d685ea4bc92987573f6
alojzy231 <alojzy231@gmail.com>
2021-08-23 15:28:59 +0200

    Updated email mockup API

--- src/clients/formcarry.js
+++ src/clients/formcarry.js
@@ -1,9 +1,23 @@
-const sendEmail = (data) =>
+const mockApi = (data, isRejected = false) =>
   new Promise((resolve, reject) => {
     setTimeout(() => {
-      reject();
+      if (isRejected) {
+        reject();
+      }
       resolve(data);
     }, 2000);
   });
+
+const sendEmail = async (data, callback, responseType) => {
+  callback(responseType.loading);
+  await mockApi(data)
+    .then((res) => {
+      callback(responseType.success);
+      console.log(res);
+    })
+    .catch(() => {
+      callback(responseType.error);
+    });
+};
 
 export default sendEmail;
