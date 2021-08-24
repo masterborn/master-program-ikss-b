7eb64b1dbf77d201ea4a14b6e78d4a7440981c00
nynon <nynonmaslo@gmail.com>
2021-08-24 12:08:43 +0200

    Added minimal page's width for mobile view

--- src/components/layout/Layout.styles.js
+++ src/components/layout/Layout.styles.js
@@ -2,8 +2,10 @@ import styled from 'styled-components';
 
 export const StyledLayout = styled.div`
   ${(props) => (props.isMobile ? '' : 'width: 1440px')};
-
-  margin: 0 auto;
+  @media (max-width: 550px) {
+    min-width: 300px;
+    width: auto;
+  }
 
   #main > div {
     display: flex;
