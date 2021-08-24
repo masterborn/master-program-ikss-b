55e3b6a9ee4c035f60801ff24ea99834f2932800
nynon <nynonmaslo@gmail.com>
2021-08-24 12:09:21 +0200

    added mobile styles

--- src/components/home/partnersList/PartnersSection.style.jsx
+++ src/components/home/partnersList/PartnersSection.style.jsx
@@ -1,5 +1,5 @@
-import { Header3 } from '@root/components/typography/headers';
-import { ParagraphBody } from '@root/components/typography/paragraphs';
+import { Header4 } from '@root/components/typography/headers';
+import { ParagraphSmall } from '@root/components/typography/paragraphs';
 import styled from 'styled-components';
 
 export const LogosSection = styled.section`
@@ -12,15 +12,33 @@ export const LogosSection = styled.section`
   margin-top: 148px;
   /* remove this margin when contact form is present on the homepage */
   margin-bottom: 400px;
+
+  @media (max-width: 550px) {
+    width: 100%;
+    max-width: 550px;
+    padding: 0 26px 0 26px;
+    height: auto;
+  } ;
 `;
-export const PartnersHeader = styled(Header3)`
+export const PartnersHeader = styled(Header4)`
   margin-bottom: 32px;
+
+  @media (max-width: 500px) {
+    margin-bottom: 16px;
+  }
 `;
 
-export const PartnersDescription = styled(ParagraphBody)`
+export const PartnersDescription = styled(ParagraphSmall)`
   text-align: center;
   width: 635px;
   height: 64px;
+
+  @media (max-width: 550px) {
+    width: 100%;
+    max-width: 550px;
+    height: auto;
+    min-height: 112px;
+  }
 `;
 
 export const LogosContainer = styled.div`
@@ -31,6 +49,14 @@ export const LogosContainer = styled.div`
   height: 336px;
   width: 1134px;
   margin-top: 80px;
+
+  @media (max-width: 550px) {
+    width: 100%;
+    max-width: 550px;
+    margin-top: 32px;
+    min-height: 302px;
+    height: auto;
+  }
 `;
 export const Logo = styled.div`
   display: flex;
@@ -38,4 +64,18 @@ export const Logo = styled.div`
   align-items: center;
   width: 226px;
   height: 112px;
+
+  @media (max-width: 550px) {
+    width: calc(100% / 3);
+    height: calc(100% / 5);
+    margin-bottom: 30px;
+
+    & a,
+    img {
+      width: 85%;
+      height: auto;
+      min-height: 35px;
+      max-height: 40px;
+    }
+  } ;
 `;
