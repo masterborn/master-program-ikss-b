7b1952c8dbc1f3936323c26aeb19a76a7e8ca131
alojzy231 <alojzy231@gmail.com>
2021-08-26 14:06:09 +0200

    'Skontaktuj się' button scrolls to contact form

--- src/components/home/topSection/index.jsx
+++ src/components/home/topSection/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { convertRichTextToReactComponent } from '@root/dataMappers/contentful';
+import { scrollToContactForm } from '@root/components/layout/navbar/contactFormButton';
 import {
   StyledTopSection,
   Content,
@@ -13,6 +14,8 @@ import {
 import SecondaryButton from '../../buttons/secondaryButton';
 
 export default function TopSection({ topSectionContent, socials }) {
+  const navbarHeight = '88px';
+
   const {
     title: sectionHeader,
     image1: { url: mediaUrl, title: mediaTitle },
@@ -21,7 +24,7 @@ export default function TopSection({ topSectionContent, socials }) {
 
   const urlIsImage = /.*.(jpg|png|jpeg)$/.test(mediaUrl);
   const Description = convertRichTextToReactComponent(Paragraph, richText);
-  const handleClick = () => {};
+  const handleClick = () => scrollToContactForm(navbarHeight);
 
   return (
     <StyledTopSection className="hideNavSocials">
--- src/components/layout/navbar/contactFormButton.jsx
+++ src/components/layout/navbar/contactFormButton.jsx
@@ -1,12 +1,16 @@
+export const scrollToContactForm = (navbarHeight) => {
+  const contactForm = document.getElementById('contact-form');
+  const navbarOffset = Number(navbarHeight.replace(/px$/, ''));
+
+  if (contactForm) {
+    const contactFormPosition = contactForm.getBoundingClientRect().top;
+    window.scrollBy(0, contactFormPosition - navbarOffset);
+  }
+};
+
 const handleClick = (currPathname, navbarHeight, toggleContactModal) => {
   if (currPathname === '/') {
-    const contactForm = document.getElementById('contact-form');
-    const navbarOffset = Number(navbarHeight.replace(/px$/, ''));
-
-    if (contactForm) {
-      const contactFormPosition = contactForm.getBoundingClientRect().top;
-      window.scrollBy(0, contactFormPosition - navbarOffset);
-    }
+    scrollToContactForm(navbarHeight);
   } else {
     toggleContactModal(true);
   }
