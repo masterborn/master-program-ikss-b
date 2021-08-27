087f80ec4bd9e97280e951813a3dd2902a81f840
alojzy231 <alojzy231@gmail.com>
2021-08-27 23:05:48 +0200

    Added simple validation

--- src/components/contactForm/validation.js
+++ src/components/contactForm/validation.js
@@ -1,40 +1,49 @@
-const validateName = (name) => {
-  if (!name) return false;
+const MAX_NAME_LENGTH = 20;
+const MIN_NAME_LENGTH = 3;
+const MAX_CONTENT_LENGTH = 150;
+const MIN_CONTENT_LENGTH = 6;
 
-  return true;
+const validateName = (name) => {
+  if (name.length < MIN_NAME_LENGTH)
+    return { isValid: false, isInvalid: true, message: 'Zbyt krótkie imię.' };
+  if (name.length > MAX_NAME_LENGTH)
+    return { isValid: false, isInvalid: true, message: 'Zbyt długi imię.' };
+
+  return { isValid: true, isInvalid: false, message: 'ok' };
 };
 const validateLastName = (lastName) => {
-  if (!lastName) return false;
+  if (lastName.length < MIN_NAME_LENGTH)
+    return { isValid: false, isInvalid: true, message: 'Zbyt krótkie nazwisko.' };
+  if (lastName.length > MAX_NAME_LENGTH)
+    return { isValid: false, isInvalid: true, message: 'Zbyt długi nazwisko.' };
 
-  return true;
+  return { isValid: true, isInvalid: false, message: 'ok' };
 };
 const validateEmail = (email) => {
-  if (!email) return false;
+  if (!email) return { isValid: false, isInvalid: true, message: 'Niepoprawny adres email.' };
 
-  return true;
+  return { isValid: true, isInvalid: false, message: 'ok' };
 };
 const validateContent = (content) => {
-  if (!content) return false;
+  if (content.length < MIN_CONTENT_LENGTH)
+    return { isValid: false, isInvalid: true, message: 'Zbyt krótki tekst.' };
+  if (content.length > MAX_CONTENT_LENGTH)
+    return { isValid: false, isInvalid: true, message: 'Zbyt długi tekst.' };
 
-  return true;
+  return { isValid: true, isInvalid: false, message: 'ok' };
 };
-const validateTermsCheckbox = (isTermsBoxChecked) => isTermsBoxChecked;
+const validateTermsCheckbox = (hasAgreedToTerms) => ({ isInvalid: !hasAgreedToTerms });
 
 const validateInputs = (formValues) => {
-  const areInputsInvalid = {};
-  const areInputsValid = {};
+  const validatedInputs = {};
 
-  areInputsValid.name = validateName(formValues.name);
-  areInputsValid.lastName = validateLastName(formValues.lastName);
-  areInputsValid.email = validateEmail(formValues.email);
-  areInputsValid.content = validateContent(formValues.content);
-  areInputsValid.termsCheckbox = validateTermsCheckbox(formValues.isTermsBoxChecked);
+  validatedInputs.name = validateName(formValues.name);
+  validatedInputs.lastName = validateLastName(formValues.lastName);
+  validatedInputs.email = validateEmail(formValues.email);
+  validatedInputs.content = validateContent(formValues.content);
+  validatedInputs.termsCheckbox = validateTermsCheckbox(formValues.hasAgreedToTerms);
 
-  Object.entries(areInputsValid).forEach(([key, value]) => {
-    areInputsInvalid[key] = !value;
-  });
-
-  return { areInputsInvalid, areInputsValid };
+  return validatedInputs;
 };
 
 export default validateInputs;
