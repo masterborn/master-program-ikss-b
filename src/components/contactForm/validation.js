0d9b098bea0b88c373f5d09144de0c08ca9fd44d
alojzy231 <alojzy231@gmail.com>
2021-08-30 09:56:27 +0200

    Added simple validation

--- src/components/contactForm/validation.js
+++ src/components/contactForm/validation.js
@@ -1,3 +1,5 @@
+import validator from 'validator';
+
 const MAX_NAME_LENGTH = 20;
 const MIN_NAME_LENGTH = 3;
 const MAX_CONTENT_LENGTH = 150;
@@ -7,7 +9,9 @@ const validateName = (name) => {
   if (name.length < MIN_NAME_LENGTH)
     return { isValid: false, isInvalid: true, message: 'Zbyt krótkie imię.' };
   if (name.length > MAX_NAME_LENGTH)
-    return { isValid: false, isInvalid: true, message: 'Zbyt długi imię.' };
+    return { isValid: false, isInvalid: true, message: 'Zbyt długie imię.' };
+  if (!validator.isAlpha(name, 'pl-PL'))
+    return { isValid: false, isInvalid: true, message: 'Wprowadzono niedozwolone znaki.' };
 
   return { isValid: true, isInvalid: false, message: 'ok' };
 };
@@ -15,12 +19,15 @@ const validateLastName = (lastName) => {
   if (lastName.length < MIN_NAME_LENGTH)
     return { isValid: false, isInvalid: true, message: 'Zbyt krótkie nazwisko.' };
   if (lastName.length > MAX_NAME_LENGTH)
-    return { isValid: false, isInvalid: true, message: 'Zbyt długi nazwisko.' };
+    return { isValid: false, isInvalid: true, message: 'Zbyt długie nazwisko.' };
+  if (!validator.isAlpha(lastName, 'pl-PL'))
+    return { isValid: false, isInvalid: true, message: 'Wprowadzono niedozwolone znaki.' };
 
   return { isValid: true, isInvalid: false, message: 'ok' };
 };
 const validateEmail = (email) => {
-  if (!email) return { isValid: false, isInvalid: true, message: 'Niepoprawny adres email.' };
+  if (!validator.isEmail(email))
+    return { isValid: false, isInvalid: true, message: 'Niepoprawny adres email.' };
 
   return { isValid: true, isInvalid: false, message: 'ok' };
 };
