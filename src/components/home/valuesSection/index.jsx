2e22bd390e8e0d390b863c4a9e47de567b1560fb
alojzy231 <alojzy231@gmail.com>
2021-09-02 18:15:13 +0200

    Added values tiles slider

--- src/components/home/valuesSection/index.jsx
+++ src/components/home/valuesSection/index.jsx
@@ -1,24 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useSelector } from 'react-redux';
 import { convertRichTextToReactComponent } from '@root/dataMappers/contentful';
-
+import ValuesTilesSlider from './valuesTilesSlider';
 import { StyledValuesSection, Title, Paragraph, TilesContainer } from './ValuesSection.styles';
 import Tile from './ValuesTile';
 
 export default function ValuesSection({ valuesText, valuesTiles }) {
   const { title, text1: richText } = valuesText;
-
+  const isMobile = useSelector((state) => state.isMobile);
   const Body = convertRichTextToReactComponent(Paragraph, richText);
   return (
     <StyledValuesSection id="values-section">
       <Title>{title}</Title>
       {Body}
-
-      <TilesContainer>
-        {valuesTiles.map((val) => (
-          <Tile data={val} key={val.title} />
-        ))}
-      </TilesContainer>
+      {isMobile ? (
+        <ValuesTilesSlider valuesTiles={valuesTiles} />
+      ) : (
+        <TilesContainer>
+          {valuesTiles.map((val) => (
+            <Tile data={val} key={val.title} />
+          ))}
+        </TilesContainer>
+      )}
     </StyledValuesSection>
   );
 }
