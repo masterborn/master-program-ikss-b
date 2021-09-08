86b9981f7c3c7efa79d520797b16694f61f807a4
nynon <nynonmaslo@gmail.com>
2021-09-08 23:41:55 +0200

    changed api limt to 1000 contentType items

--- src/clients/contentful.js
+++ src/clients/contentful.js
@@ -2,7 +2,7 @@ import config from '@root/config/contentful';
 import data from '../api/data.json';
 
 export default async function getPagesData() {
-  const apiEndPoint = `https://cdn.contentful.com/spaces/${config.SPACE_ID}/environments/master/entries?access_token=${config.TOKEN}`;
+  const apiEndPoint = `https://cdn.contentful.com/spaces/${config.SPACE_ID}/environments/master/entries?access_token=${config.TOKEN}&limit=1000`; // yeah that's it
   const timeoutId = setTimeout(() => {
     throw new Error('API request timeout');
   }, 5000);
