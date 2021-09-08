45c893ae6b08f9387dd3eb347b6fd85610123016
alojzy231 <alojzy231@gmail.com>
2021-09-08 14:06:20 +0200

    Destructured props

--- src/components/home/valuesSection/ValuesTile.styles.js
+++ src/components/home/valuesSection/ValuesTile.styles.js
@@ -14,7 +14,7 @@ export const StyledTile = styled.div`
 
   z-index: 0;
 
-  background-color: ${(props) => props.theme.color.white};
+  background-color: ${({ theme: { color } }) => color.white};
   box-shadow: 3.38443px 55.8976px 80px rgba(97, 121, 139, 0.07),
     1.71337px 28.2982px 34.875px rgba(97, 121, 139, 0.04725),
     0.676885px 11.1795px 13px rgba(97, 121, 139, 0.035),
@@ -75,7 +75,7 @@ export const Title = styled(Header5)`
   margin: 20px 24px 0;
 
   height: 72px;
-  color: ${(props) => props.theme.color.navy};
+  color: ${({ theme: { color } }) => color.navy};
   overflow: hidden;
 
   @media (max-width: 550px) {
@@ -92,7 +92,7 @@ export const Paragraph = styled(ParagraphBody)`
   overflow: hidden;
 
   text-align: center;
-  color: ${(props) => props.theme.color.steel};
+  color: ${({ theme: { color } }) => color.steel};
 
   @media (max-width: 550px) {
     margin: 12px 20px 0;
