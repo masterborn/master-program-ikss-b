f4d8407821356161ac21fc011364e462ff58f13f
nynon <nynonmaslo@gmail.com>
2021-09-14 12:13:46 +0200

    smal fix to board Member tile styles

--- src/components/aboutUs/boardMembersSection/boardMemberTile/TileAnimations.styles.js
+++ src/components/aboutUs/boardMembersSection/boardMemberTile/TileAnimations.styles.js
@@ -2,20 +2,21 @@ import { css } from 'styled-components';
 
 const toggleTiletransition = css`
   transition: opacity 0.2s linear, height 0.1s linear;
-`;
+`.join('');
 
 const hideTile = css`
   height: 0;
   padding: 0;
   opacity: 0;
   ${toggleTiletransition}
-`;
-const showTile = (componentHeight, componentPadding) => css`
-  height: ${componentHeight};
-  padding: ${componentPadding};
-  opacity: 1;
-  ${toggleTiletransition}
-`;
+`.join('');
+const showTile = (componentHeight, componentPadding) =>
+  css`
+    height: ${componentHeight};
+    padding: ${componentPadding};
+    opacity: 1;
+    ${toggleTiletransition}
+  `.join('');
 
 const toggleTileVisibility = (shouldDisplay, componentHeight, componentPadding = 0) =>
   shouldDisplay ? showTile(componentHeight, componentPadding) : hideTile;
