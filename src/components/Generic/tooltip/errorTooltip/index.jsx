25284c400d156be9057873e0d949a015ca971e93
alojzy231 <alojzy231@gmail.com>
2021-09-28 11:46:54 +0200

    Added animation to an info and an error tooltip

--- src/components/Generic/tooltip/errorTooltip/index.jsx
+++ src/components/Generic/tooltip/errorTooltip/index.jsx
@@ -4,9 +4,8 @@ import PropTypes from 'prop-types';
 import { ErrorTooltipContainer, Text, ErrorTooltipTail } from './ErrorTooltip.styles';
 
 export default function ErrorTooltip({ tooltipText, show }) {
-  if (!show) return null;
   return (
-    <ErrorTooltipContainer>
+    <ErrorTooltipContainer show={show}>
       <Text>{tooltipText}</Text>
       <ErrorTooltipTail />
     </ErrorTooltipContainer>
